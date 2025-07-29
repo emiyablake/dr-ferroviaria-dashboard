0a06bb35f78ef385efd211dcfeaa21d316342105
emiyablake <maju.ads.02@gmail.com>
2025-07-28 22:24:32 -0300

    create dash

--- pages/showdataframe.py
+++ pages/showdataframe.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd
-from . import session
+import os
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from models import Patio, Terminal, Mercadoria, TerminalMercadoria, Base 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from models import Patio, Terminal, Mercadoria, TerminalMercadoria, EntrePatio
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, '..', 'db', 'ferroviaria.db')
+
+engine = create_engine(f"sqlite:///{DB_PATH}")
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 def run():
@@ -12,6 +24,11 @@ def run():
     patios = session.query(Patio).all()
     df_patio = pd.DataFrame([p.__dict__ for p in patios]).drop(columns=["_sa_instance_state"])
     st.dataframe(df_patio)
+
+    st.subheader("Tabela: Entre_patios")
+    entre_patio = session.query(EntrePatio).all()
+    df_entrePatio = pd.DataFrame([p.__dict__ for p in entre_patio]).drop(columns=["_sa_instance_state"])
+    st.dataframe(df_entrePatio)
 
     st.subheader("Tabela: Terminal")
     terminais = session.query(Terminal).all()
@@ -27,3 +44,6 @@ def run():
     terminal_mercadoria = session.query(TerminalMercadoria).all()
     df_terminalMercadoria = pd.DataFrame([p.__dict__ for p in terminal_mercadoria]).drop(columns=["_sa_instance_state"],  errors="ignore")
     st.dataframe(df_terminalMercadoria)
+
+if __name__ == "__main__":
+    run()
